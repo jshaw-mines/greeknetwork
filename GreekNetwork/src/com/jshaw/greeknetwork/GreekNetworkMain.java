4dafee3ccc0e4a2cc601ef666b6f3ac23303cd67
Joshua Shaw <jshaw@mines.edu>
2012-12-03 10:47:44 -0700

    added functionality to main menu buttons, added skeleton members list

--- GreekNetwork/src/com/jshaw/greeknetwork/GreekNetworkMain.java
+++ GreekNetwork/src/com/jshaw/greeknetwork/GreekNetworkMain.java
@@ -3,6 +3,9 @@ package com.jshaw.greeknetwork;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class GreekNetworkMain extends Activity {
 
@@ -10,6 +13,17 @@ public class GreekNetworkMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event);
+        
+        Button a = (Button)findViewById(R.id.view_member_list);
+        a.setOnClickListener(new OnClickListener() 
+        {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(GreekNetworkMain.this, MemberList.class);
+				startActivity(i);
+			}	
+        });
+        Button b = (Button)findViewById(R.id.view_event_list);
     }
 
     @Override
